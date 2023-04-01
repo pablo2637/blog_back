10e9873725a09b5daf1f1d1467dd19879e978464
pablo2637 <pabli2637@gmail.com>
2023-04-01 18:43:02 +0200

    base del proyecto

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+port = process.env.PORT || 3000;
+
+app.use(cors());                                    //Cors
+app.use(express.static(__dirname + '/public'));     //Carpeta static
+
+app.set('view engine', 'ejs');                      //Template engine
+app.set('views', __dirname + '/views');
+
+app.use(express.urlencoded({ extended: false }))    // Parse application/x-www-form-urlencoded
+app.use(express.json())                             // Parse application/json
+
+//Rutas
+
+
+//404
+
+
+app.listen(port, () => {
+    console.log(`Server listenning on port ${port}...`);
+});
