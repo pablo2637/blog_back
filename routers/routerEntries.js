56619c2126633fe93ea293815ae9e01cfd6846ea
pablo2637 <pabli2637@gmail.com>
2023-04-03 12:31:33 +0200

    entries y validaciones

---
+++ routers/routerEntries.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+
+const { check } = require('express-validator');
+const { validateInputs } = require('../middlewares/validarInputs');
+
+const {
+    getEntries,
+    getEntriesByEmail,
+    getEntryByID,
+    createEntry,
+    updateEntry,
+    deleteEntry } = require('../controllers/controllerApiEntries');
+
+
+router.get('/', getEntries);
+
+
+router.get('/email/:email', getEntriesByEmail);
+
+
+router.get('/id/:id', getEntryByID);
+
+
+router.post('/', [
+    check('title', 'El título es obligatorio').trim().not().isEmpty(),
+    check('content', 'La descripción es obligatoria').trim().not().isEmpty(),
+    check('extract', 'El extracto es obligatorio').trim().not().isEmpty(),
+    check('image', 'La imagen es obligatoria').trim().not().isEmpty(),
+    validateInputs
+], createEntry);
+
+
+router.put('/', [
+    check('title', 'El título es obligatorio').trim().not().isEmpty(),
+    check('content', 'La descripción es obligatoria').trim().not().isEmpty(),
+    check('extract', 'El extracto es obligatorio').trim().not().isEmpty(),
+    check('image', 'La imagen es obligatoria').trim().not().isEmpty(),
+], updateEntry);
+
+
+router.delete('/:entryID', deleteEntry);
+
+
+module.exports = router
